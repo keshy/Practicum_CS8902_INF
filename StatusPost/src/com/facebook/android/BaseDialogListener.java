8429606d7ffec236fa9c565d4d27f305ebb24d26
keshi8086 <keshi8086@4326ef04-9603-d15b-c388-a3474ffdb81f>
2011-06-06 15:21:05 +0000

    facebook sdk for android for the bluetooth project.

---
+++ StatusPost/src/com/facebook/android/BaseDialogListener.java
@@ -0,0 +1,25 @@
+package com.facebook.android;
+
+import com.facebook.android.framework.DialogError;
+import com.facebook.android.framework.FacebookError;
+import com.facebook.android.framework.Facebook.DialogListener;
+
+/**
+ * Skeleton base class for RequestListeners, providing default error 
+ * handling. Applications should handle these error conditions.
+ *
+ */
+public abstract class BaseDialogListener implements DialogListener {
+
+    public void onFacebookError(FacebookError e) {
+        e.printStackTrace();
+    }
+
+    public void onError(DialogError e) {
+        e.printStackTrace();        
+    }
+
+    public void onCancel() {        
+    }
+    
+}
